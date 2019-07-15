53fe007f34e40eea3498d3dd36b0f26ac91a100d
mutterer <jmutterer@skopien.ibmp.unistra.fr>
2019-07-15 15:12:34 +0200

    add vincent d1 shutter with neopixel example

---
+++ MM_VincentD1_shutter/MM_VincentD1_shutter.ino
@@ -0,0 +1,56 @@
+// An arduino firmware that can be used with the Micro-Manager Vincent D1 shutter device adaptor.
+// version 0.1
+// JM 
+// References
+// https://www.micro-manager.org/wiki/Vincent
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+#define PIN            3
+#define NUMPIXELS      8
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+
+String cmd = "" ;
+int a = 100;     // a could be linked to a potentiometer on the device.
+boolean state = false;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setTimeout(20); // no line ending so rely on timeout
+  pixels.begin();
+}
+
+void loop()
+{
+  if (Serial.available()) {
+    cmd = Serial.readString(); // no line ending so rely on timeout
+    processCommand(cmd);
+    cmd = "";
+  }
+}
+
+// only open, close and reset are implemented here
+void processCommand(String s) {
+  if (s.startsWith("@")) {
+   state = true;
+  } else if (s.startsWith("A")) {
+       state = false;
+
+  } else if (s.startsWith("C")) {
+       state = false;
+
+  } 
+  // update pixels according to state and intensity
+  // MM sends a [0..100] value, map this to [0..255]
+  for (int i = 0; i < NUMPIXELS; i++) {
+    double f = 2.55*state; 
+    // a could be linked to a potentiometer on the device.
+    pixels.setPixelColor(i, pixels.Color(f*a, f*a, f*a));
+  }
+  pixels.show();
+}
+
